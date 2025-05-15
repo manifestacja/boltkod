80c87856b982e6e8d97e87c83147acd7a9378647
manifestacja <manifestacja7@gmail.com>
2025-05-15 08:21:54 +0200

    Update google.ts

--- app/lib/modules/llm/providers/google.ts
+++ app/lib/modules/llm/providers/google.ts
@@ -9,7 +9,7 @@ export default class GoogleProvider extends BaseProvider {
   getApiKeyLink = 'https://aistudio.google.com/app/apikey';
 
   config = {
-    apiTokenKey: 'GOOGLE_GENERATIVE_AI_API_KEY',
+    apiTokenKey: 'AIzaSyB57k6FEah9BxAkfAYRmboKI9xhIRxxvNE',
   };
 
   staticModels: ModelInfo[] = [
@@ -38,7 +38,7 @@ export default class GoogleProvider extends BaseProvider {
       providerSettings: settings,
       serverEnv: serverEnv as any,
       defaultBaseUrlKey: '',
-      defaultApiTokenKey: 'GOOGLE_GENERATIVE_AI_API_KEY',
+      defaultApiTokenKey: 'AIzaSyB57k6FEah9BxAkfAYRmboKI9xhIRxxvNE',
     });
 
     if (!apiKey) {
@@ -76,7 +76,7 @@ export default class GoogleProvider extends BaseProvider {
       providerSettings: providerSettings?.[this.name],
       serverEnv: serverEnv as any,
       defaultBaseUrlKey: '',
-      defaultApiTokenKey: 'GOOGLE_GENERATIVE_AI_API_KEY',
+      defaultApiTokenKey: 'AIzaSyB57k6FEah9BxAkfAYRmboKI9xhIRxxvNE',
     });
 
     if (!apiKey) {
